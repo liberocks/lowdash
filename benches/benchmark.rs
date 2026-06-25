fa3bb73e13549052a16ddc83454a66c49773c69e
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2026-06-25 15:22:35 +0700

    bench: clean generated benchmarks

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,4 +1,4 @@
-        use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 mod assign;
 mod associate;
@@ -270,9 +270,9 @@ fn all_benches(c: &mut Criterion) {
     words::benchmark_words(c);
 }
 
-        criterion_group! {
-            name = benches;
-            config = custom_criterion();
-            targets = all_benches
-        }
-        criterion_main!(benches);
+criterion_group! {
+    name = benches;
+    config = custom_criterion();
+    targets = all_benches
+}
+criterion_main!(benches);
