616a7267911136d93deb96a84fab00e92fa1b6bc
liberocks <tirtadwipa.manunggal@gmail.com>
2026-08-02 18:04:43 +0700

    feat: add sort_by_key utility

---
+++ benches/sort_by_key.rs
@@ -0,0 +1,30 @@
+use crate::support;
+use criterion::{black_box, Criterion};
+use lowdash as ld;
+
+pub fn benchmark_sort_by_key(c: &mut Criterion) {
+    let people = support::people_shuffled(4_096);
+    c.bench_function("sort_by_key/people_shuffled", |b| {
+        b.iter(|| {
+            ld::sort_by_key(
+                black_box(&people),
+                black_box(|person: &support::Person| person.age),
+            )
+        })
+    });
+
+    let numbers = support::int_vec_shuffled(4_096);
+    c.bench_function("sort_by_key/int_vec_shuffled", |b| {
+        b.iter(|| ld::sort_by_key(black_box(&numbers), black_box(|number: &i32| *number)))
+    });
+
+    let equal_age_people = support::people_same_age(4_096);
+    c.bench_function("sort_by_key/equal_keys", |b| {
+        b.iter(|| {
+            ld::sort_by_key(
+                black_box(&equal_age_people),
+                black_box(|person: &support::Person| person.age),
+            )
+        })
+    });
+}
