33107d8aac4bff44997db87ee955bb9ecb159069
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2026-08-08 21:32:46 +0700

    feat(statistics): add median high

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -95,6 +95,7 @@ mod max_by_key;
 mod mean;
 mod mean_by;
 mod median;
+mod median_high;
 mod median_low;
 mod min;
 mod min_by;
@@ -274,6 +275,7 @@ fn all_benches(c: &mut Criterion) {
     mean::benchmark_mean(c);
     mean_by::benchmark_mean_by(c);
     median::benchmark_median(c);
+    median_high::benchmark_median_high(c);
     median_low::benchmark_median_low(c);
     min::benchmark_min(c);
     min_by::benchmark_min_by(c);
