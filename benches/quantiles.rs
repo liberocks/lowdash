b9b26996aecc10b6c0141282a05c9a0e9c34faa7
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2026-08-08 21:36:24 +0700

    feat(statistics): add quantiles

---
+++ benches/quantiles.rs
@@ -0,0 +1,9 @@
+use criterion::{black_box, Criterion};
+use lowdash as ld;
+
+pub fn benchmark_quantiles(c: &mut Criterion) {
+    let values: Vec<f64> = (0..4_096).map(|value| value as f64 / 3.0).collect();
+    c.bench_function("quantiles/quartiles", |b| {
+        b.iter(|| ld::quantiles(black_box(&values), black_box(4)))
+    });
+}
